25c7c74d8370a405f492ca8a612d9c92ddaf554b
Javier Fernandez <fdezgj39@gmail.com>
2021-04-09 14:16:31 +0200

    Mas Cambios

---
+++ actividad4/src/prueba1/Main.java
@@ -0,0 +1,13 @@
+package prueba1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pruebas git.");
+		System.out.println("Hola git.");
+		System.out.println("Hola mundo git.");
+
+	}
+
+}
